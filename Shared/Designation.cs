fb246de360dd7f8db1884fa8f88f114c1d41c795
Nabin Kandel <nkandel209@gmail.com>
2022-05-04 00:57:42 +0545

    blazor project

---
+++ Shared/Designation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAppMudBlazorProject.Shared;
+
+public class Designation
+{
+    public int ID { get; set; }
+    public string DesignationName { get; set; }
+}
